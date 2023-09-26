25e813143ae2e9825c1003b453fd41515e591ae2
dityuiri <920aditya.putra@gmail.com>
2023-09-26 13:55:30 +0700

    Add user table sql script

--- database.sql
+++ database.sql
@@ -15,3 +15,15 @@ CREATE TABLE test (
 
 INSERT INTO test (name) VALUES ('test1');
 INSERT INTO test (name) VALUES ('test2');
+
+CREATE TABLE IF NOT EXISTS user (
+    id   UUID  PRIMARY KEY,
+    phone_number VARCHAR(13) NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    password_hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
+
+    CONSTRAINT phone_number_key UNIQUE(phone_number)
+);
+
+CREATE INDEX idx_user_phone_number ON user(phone_number);
